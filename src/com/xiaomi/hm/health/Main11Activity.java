111ec87f0affb137d4b78164d99955719a42524d
pangliang <418094911@qq.com>
2016-05-11 17:59:27 +0800

    Update Main11Activity.java
    删掉错误提交的多余代码

--- src/com/xiaomi/hm/health/Main11Activity.java
+++ src/com/xiaomi/hm/health/Main11Activity.java
@@ -11,7 +11,5 @@ public class Main11Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main11);
-		
-		BluetoothGatt
 	}
 }
